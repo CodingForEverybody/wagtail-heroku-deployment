c3abef58f3e8ba43fcadad6b5142e29a5e9ce157
Kalob Taulien <kalob.taulien@gmail.com>
2019-05-23 08:40:15 -0600

    Typo fix

--- project_title/settings/base.py
+++ project_title/settings/base.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 
-    'whitenoise.middleware.WhiteNoiseMiddleware’,
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
