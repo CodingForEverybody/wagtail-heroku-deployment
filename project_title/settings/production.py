2abb6630d2c578d3c06de58230ff69456217eb43
Kalob Taulien <kalob.taulien@gmail.com>
2019-05-23 08:32:16 -0600

    Removed space

--- project_title/settings/production.py
+++ project_title/settings/production.py
@@ -10,7 +10,7 @@ DEBUG = False
 
 # Parse database configuration from $DATABASE_URL
 DATABASES['default'] =  dj_database_url.config()
-    
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
